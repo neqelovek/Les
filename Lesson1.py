e10618d7f19484868b405654421456173a651fb0
neqelovek <neqelovek@gmail.com>
2022-10-14 01:36:30 +0300

    Задание 2 готово

--- Lesson1.py
+++ Lesson1.py
@@ -8,17 +8,40 @@
 # - 1 -> нет
 
 
-from pdb import Restart
+# from pdb import Restart
 
 
-number = int(input('Введите число от 1 до 7 '))
+# number = int(input('Введите число от 1 до 7 '))
 
-if number >= 1 and number <= 5:
-    print('Это рабочий день ')
-elif number == 6 or number == 7:
-    print('Это выходной ')
+# if number >= 1 and number <= 5:
+#     print('Это рабочий день ')
+# elif number == 6 or number == 7:
+#     print('Это выходной ')
+# else:
+#     print('Не правильно ввели число!!!') 
+
+
+  # Task 2
+     # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
 else:
-    print('Не правильно ввели число!!!') 
-
-    # Task 2
-    
+    print(f"Утверждение ложно")
