785931cb6bdf9a17803aa0b19be0f887d84f02b1
neqelovek <neqelovek@gmail.com>
2022-11-03 01:20:39 +0300

    Task 2 finish

--- Lesson3.py
+++ Lesson3.py
@@ -5,12 +5,12 @@
 # *Пример:*
 
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
-list = [2, 3, 5, 9, 3, 2, 4, 2, 1.2, 4.3]
-list2 = []
-for i in range(len(list)):
-    if i % 2 !=0:
-        list2.append(list[i])
-print(sum(list2))
+# list = [2, 3, 5, 9, 3, 2, 4, 2, 1.2, 4.3]
+# list2 = []
+# for i in range(len(list)):
+#     if i % 2 !=0:
+#         list2.append(list[i])
+# print(sum(list2))
 
 
 # Задача 2.
@@ -21,3 +21,18 @@ print(sum(list2))
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+
+def sum_numbers(num, resoult):
+    if num[0] != num[-1]:
+        for i in num:
+            delete_numbers = num[0] * num[-1]
+            num.pop(0)
+            num.pop(-1)
+            resoult.append(delete_numbers)
+    else: num[0] == num[-1]
+    delete_numbers = num[0] * num[-1]       
+    resoult.append(delete_numbers)
+    print(resoult)
+
+sum_numbers([2, 3, 4, 5, 6], [])
