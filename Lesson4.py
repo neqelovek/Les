fde55b923756fcc88832b7c91e24f7fb60845986
neqelovek <neqelovek@gmail.com>
2022-11-08 01:46:08 +0300

    task 3 finish

--- Lesson4.py
+++ Lesson4.py
@@ -24,18 +24,53 @@
 #            которая выведет список неповторяющихся элементов исходной последовательности.
 
 
-def non_repeating_numbers(sequence_numbers):
-    resoult = []
+# def non_repeating_numbers(sequence_numbers):
+#     resoult = []
 
-    for number in sequence_numbers:
-        if number in resoult:
-            continue
-        else:
-            resoult.append(number)
-    return print(resoult)
+#     for number in sequence_numbers:
+#         if number in resoult:
+#             continue
+#         else:
+#             resoult.append(number)
+#     return print(resoult)
 
-non_repeating_numbers([9, 2, 2, 3, 4, 5, 5, 2, 6, 7, 1, 9])
+# non_repeating_numbers([9, 2, 2, 3, 4, 5, 5, 2, 6, 7, 1, 9])
 
 
+# задача 3. Задана натуральная степень k. 
+#            Сформировать случайным образом список коэффициентов (значения от 0 до 100)
+#            многочлена и записать в файл многочлен степени k.
 
+# *Пример:* 
+
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+import itertools
+
+k = 2
+
+def get_ratios(k):
+    ratios = [randint(0, 10) for i in range (k + 1)]
+    while ratios[0] == 0:
+        ratios[0] = randint(1, 10) 
+    return ratios
+
+def get_polynomial(k, ratios):
+    var = ['*x^']*(k-1) + ['*x']
+    polynomial = [[a, b, c] for a, b, c \
+    in itertools.zip_longest(ratios, var, range(k, 1, -1),fillvalue = '') if a !=0]
+    for x in polynomial:
+        x.append(' + ')
+    polynomial = list(itertools.chain(*polynomial))
+    polynomial[-1] = ' = 0'
+    return "".join(map(str, polynomial)).replace(' 1*x',' x')
+
+
+ratios = get_ratios(k)
+polynom1 = get_polynomial(k, ratios)
+print(polynom1)
+
+with open('Polynomial.txt', 'w') as data:
+    data.write(polynom1)
 
